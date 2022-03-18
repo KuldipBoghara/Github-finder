926268e6d5de895c28f9f590c30ab14b13ce2722
Mr-BK <bogharakuldip@gmail.com>
2022-03-18 12:19:59 -0400

    contex

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
+import GithubContext from '../../context/github/GithubContex';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
+  const { users, loading } = useContext(GithubContext);
+
+  /* const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
+  //Fetch Users
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: { Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` }
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` }
     });
 
     const data = await response.json();
     setUsers(data);
     setLoading(false);
-  };
+  }; */
 
+  /*   //for testing (displays all users)
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+ */
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
--- src/context/github/GithubContex.jsx
+++ src/context/github/GithubContex.jsx
@@ -1,8 +1,77 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './Githubreducer';
 
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-export const GithubProvider = () => {};
+//provider function
+export const GithubProvider = ({ children }) => {
+  /*   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true); */
+
+  const initialState = {
+    users: [],
+    loading: false
+  };
+
+  //useReducer
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  //Set Loading
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
+
+  /* //Fetch Users (for testing , gets initial users)
+  const fetchUsers = async () => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` }
+    });
+
+    const data = await response.json();
+    dispatch({
+      type: 'GET_USERS',
+      payload: data
+    });
+  }; */
+
+  //Search Users
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` }
+    });
+
+    const { items } = await response.json();
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: items
+    });
+  };
+
+  //clear Users
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
